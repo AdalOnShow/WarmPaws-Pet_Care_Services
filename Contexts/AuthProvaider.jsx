a59c76b7cdf73399335d10ebe9cd70ffb7f1bfce
Sharif H. Adal <mdadalbadsha2008@gmail.com>
2025-10-22 12:41:54 +0600

    feat: add Google sign-in functionality and update authentication context

--- Contexts/AuthProvaider.jsx
+++ Contexts/AuthProvaider.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth'
 import { auth } from '../Firebase/firebase'
 import { AuthContext } from './AuthContext'
 
 const AuthProvaider = ({ children }) => {
   const [user, setUser] = useState(null)
   console.log(user)
+
+  const googleProvider = new GoogleAuthProvider()
 
   const registerWithEmailAndPassword = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password)
@@ -15,16 +17,32 @@ const AuthProvaider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password)
   }
 
+  const sigInWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider)
+  }
+
   const updateProfileFunc = (name, photo) => {
     return updateProfile(auth.currentUser, { displayName: name, photoURL: photo })
   }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currUser) => {
+      setUser(currUser)
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
 
   const authInfo = {
     user,
     setUser,
     registerWithEmailAndPassword,
     updateProfileFunc,
-    signInWithEmailAndPasswordFunc
+    signInWithEmailAndPasswordFunc,
+    sigInWithGoogle
   }
 
   return <AuthContext value={authInfo}>{children}</AuthContext>
